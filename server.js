963bc70799728cef2abc9f8e1c122961ca16fa92
fewb92 <sotojosh96@gmail.com>
2021-07-16 19:06:19 -0400

    bug fixes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
